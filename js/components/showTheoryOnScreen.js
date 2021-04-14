86fc98a66df67e585694768bb60d0b93750824e9
Eduh Guarieiro <eduguarieiro@gmail.com>
2021-04-13 21:28:36 -0300

    Add a new feature to remove the inputs after the measure has been added

--- js/components/showTheoryOnScreen.js
+++ js/components/showTheoryOnScreen.js
@@ -1,11 +1,16 @@
 var k = 1
+var j = 0
 var html = ""
 
-function showTheoryOnScreen(medida){
+function showTheoryOnScreen(medida, measure){
     var body = document.getElementById('body')
-    html += '<tr><td id="valorMedida['+ i +']">' + medida + '</td>'
-    html += '<td><input id="input" type="number"></input></td>'
-    html += '<td></td>'
-    html += '<td></td></tr>'
+    html += '<tr><td id="valorMedida['+ k +']">' + medida + '</td>'
+    html += '<td id="tdInput['+ j +']"><input id="input['+ k +']" type="number"></td>'
     body.innerHTML = html
+    if(j>0){
+        for (var t = 0; t < measure.length; t++){
+        let input = document.getElementById('tdInput['+ t +']')
+        input.innerHTML = measure[t]
+        }
+    }
 }
