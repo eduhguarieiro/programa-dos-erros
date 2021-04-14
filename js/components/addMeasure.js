935fa7b5419e266ca7e1dc886fc053bf643747b5
Eduh Guarieiro <eduguarieiro@gmail.com>
2021-04-13 21:26:51 -0300

    Fixed the comunication problem with the others js components

--- js/components/addMeasure.js
+++ js/components/addMeasure.js
@@ -1,10 +1,16 @@
 var i = 0
 var measure = []
+var pressed = 0
 
-function addMeasure(medida){
-    measure[i] = document.getElementById('input').value
-    medida = document.getElementById('valorMedida[' + i + ']').textContent
+function addMeasure(numMedida){
+    let input = document.getElementById('input['+ k +']')
+    measure[i] = input.value
+    numMedida = document.getElementById('valorMedida[' + k + ']').textContent
     i++
-    medida++
-    showTheoryOnScreen(medida)
+    j++
+    k++
+    numMedida++
+    pressed++
+    showTheoryOnScreen(numMedida, measure)
+    document.getElementById('execute').disabled = false
 }
