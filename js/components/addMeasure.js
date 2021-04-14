2c7f2ff6130e363487a5f20ca78210ff53cfcdc6
Eduh Guarieiro <eduguarieiro@gmail.com>
2021-04-14 17:44:42 -0300

    Create a validation process in addMeasure.js

--- js/components/addMeasure.js
+++ js/components/addMeasure.js
@@ -4,13 +4,18 @@ var pressed = 0
 
 function addMeasure(numMedida){
     let input = document.getElementById('input['+ k +']')
-    measure[i] = input.value
-    numMedida = document.getElementById('valorMedida[' + k + ']').textContent
-    i++
-    j++
-    k++
-    numMedida++
-    pressed++
-    showTheoryOnScreen(numMedida, measure)
-    document.getElementById('execute').disabled = false
+    if(input.value != ""){
+        measure[i] = input.value
+        numMedida = document.getElementById('valorMedida[' + k + ']').textContent
+        i++
+        j++
+        k++
+        numMedida++
+        pressed++
+        showTheoryOnScreen(numMedida, measure)
+        document.getElementById('execute').disabled = false
+    }
+    else{
+        alert("Por favor, insira uma medida!")
+    }
 }
