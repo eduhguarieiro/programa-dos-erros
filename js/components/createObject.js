b1f53d3497dca9114dfebb5fabfeeb7be59f184e
Eduh Guarieiro <eduguarieiro@gmail.com>
2021-04-17 01:52:29 -0300

    Add ordem atribute in var medidas

--- js/components/createObject.js
+++ js/components/createObject.js
@@ -1,10 +1,11 @@
 function createObject(measure, desvAb, desvAb2){
     let medidas = []
     for(let g = 0; g < measure.length; g++){
-        medidas[g] = {medida: measure[g],
+        medidas[g] = { ordem: g + 1,
+        medida: measure[g],
         desvAb: desvAb[g],
         desvAb2: desvAb2[g]
         }
     }
-    console.log(medidas)
+    return medidas
 }
