f2ac93645efc69cbd266776164b50b3fd1008f5b
Eduh Guarieiro <eduguarieiro@gmail.com>
2021-04-17 01:49:47 -0300

    Create the showResultsOfTheory.js component

---
+++ js/components/showResultsOfTheory.js
@@ -0,0 +1,21 @@
+function showResultsOfTheory(medidas, media, desvPad){
+    let html = ""
+    let section = document.getElementById('section')
+    section.innerHTML = html
+    html += '<div class="container">'
+    html += '<table id="table" class="main-table"><thead><tr><th>N</th>'
+    html += '<th>Medida</th>'
+    html += '<th>Desvio Absoluto</th>'
+    html += '<th>Desvio Absoluto²</th>'
+    html += '<tbody>'
+    for(let h = 0; h < medidas.length; h++){
+        html += '<tr><td>' + medidas[h].ordem + '</td>'
+        html += '<td>' + medidas[h].medida + '</td>'
+        html += '<td>' + medidas[h].desvAb + '</td>'
+        html += '<td>' + medidas[h].desvAb2 + '</td></tr>'
+    }
+    html += '</tbody></table></div>'
+    html += '<h2 class="main-subtitle">A medida é (' + media + '±' + desvPad + ')unidades<h2>'
+    section.innerHTML = html
+    document.getElementById('theory').disabled = false
+}
