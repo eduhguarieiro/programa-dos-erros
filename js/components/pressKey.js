fa40cc76fc95e5d035298ed6128d94f6fbddd4a3
Eduh Guarieiro <eduguarieiro@gmail.com>
2021-04-21 02:28:38 -0300

    Add functioning for erase and clear buttons

--- js/components/pressKey.js
+++ js/components/pressKey.js
@@ -1,7 +1,17 @@
 var html2 = ""
+var timer = 0
 
 function pressKey(key){
     let display = document.getElementById('calculation')
-    html2 += key
+    if(key == 'erase'){
+        let calculation = display.textContent
+        html2 = calculation.substring(0, calculation.length - 1)
+    }
+    else if(key == 'clear'){
+        html2 = ""
+    }
+    else{
+        html2 += key
+    }
     display.innerHTML = html2
 }
--- js/components/propagationClick.js
+++ js/components/propagationClick.js
@@ -3,7 +3,9 @@ function propagationClick(){
     let html = ""
     section.innerHTML = html
     html += '<div class="main-calculator">'
-    html += '<div class="wrapper"><div id="display" class="display"><p id="calculation" class="text"></p></div>'
+    html += '<div class="wrapper"><div id="display" class="display">'
+    html += '<button class="clear" onClick="pressKey(`clear`)">Clear</button>'
+    html += '<p id="calculation" class="text"></p></div>'
     html += '<div class="keyboard">'
     html += '<div class="numbers"><button class="button" onClick="pressKey(7)">7</button>'
     html += '<button class="button" onClick="pressKey(8)">8</button>'
@@ -16,8 +18,8 @@ function propagationClick(){
     html += '<button class="button" onClick="pressKey(3)">3</button><br>'
     html += '<button class="button -bdl" onClick="pressKey(0)">0</button>'
     html += '<button class="button" onClick="pressKey(`.`)">.</button>'
-    html += '<button class="button">=</button></div>'
-    html += '<div class="operations"><button class="button -minor" onClick="buttonErase()">⌫</button>'
+    html += '<button class="button" onClick="pressKey(`equal`)">=</button></div>'
+    html += '<div class="operations"><button id="erase" class="button -minor" onClick="pressKey(`erase`)">⌫</button>'
     html += '<button class="button -minor" onClick="pressKey(`±`)">±</button>'
     html += '<button class="button -minor" onClick="pressKey(`÷`)">÷</button>'
     html += '<button class="button -minor" onClick="pressKey(`×`)">×</button>'
